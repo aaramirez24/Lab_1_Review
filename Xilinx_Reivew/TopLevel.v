66f34c77407acfb39f3983a3073f8ded4f3f4660
aaramirez24 <aaramirez24@gmail.com>
2015-01-12 16:22:59 -0800

    outputs AND from SW0 & SW1
    Testing Code

---
+++ Xilinx_Reivew/TopLevel.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:06:06 01/12/2015 
+// Design Name: 
+// Module Name:    TopLevel 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module TopLevel(A,B,C
+    );
+	input A, B;
+	output C;
+	and(C, A, B);
+
+endmodule
